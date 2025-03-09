a303c009a5278773386d7491cd5bfe183fb6acde
NicoutG <nicogallet01@gmail.com>
2025-03-09 10:59:47 +0100

    virer inutiles

--- src/MF.java
+++ src/MF.java
@@ -32,8 +32,6 @@ public class MF extends JFrame implements Observer{
 
     private Joueur joueur2 = new Humain();
 
-    private long start;
-
     /**
      * Constructeur de MF
      */
@@ -41,7 +39,6 @@ public class MF extends JFrame implements Observer{
         // Construit l'interface graphique
         build();
         echecs = eche;
-        start = System.currentTimeMillis();
     }
 
     /**
@@ -211,8 +208,6 @@ public class MF extends JFrame implements Observer{
             case 2: System.out.println("Victoire des noirs");break;
             case 3: System.out.println("Egalité");break;
         }
-        if (echecs.getVictoire() != 0)
-            System.out.println(System.currentTimeMillis() - start);
         if (echecs.getOrdre()==0) {
             // évaluation
             double eval=echecs.evaluation();
